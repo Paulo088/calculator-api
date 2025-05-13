fbe459eaa2b1aac4ac38a65e0870907be9ea4e28
Paulo Roberto Barbosa de França <p.barbosa088@gmail.com>
2025-05-12 21:38:21 -0300

    fix: adjusts type in CalculationRequest

--- src/main/java/com/paulo/calculator/dtos/CalculationRequest.java
+++ src/main/java/com/paulo/calculator/dtos/CalculationRequest.java
@@ -6,10 +6,10 @@ import jakarta.validation.constraints.NotNull;
 public class CalculationRequest {
 
     @NotNull(message = "Operand 1 is required")
-    private double operand1;
+    private Double operand1;
 
     @NotNull(message = "Operand 2 is required")
-    private double operand2;
+    private Double operand2;
 
     @NotNull(message = "Operation is required")
     private OperationType operation;
